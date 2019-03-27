ed915f1c7a003c634e02151d288abe5da39c36b1
panikaxa <zheka.9009@yandex.ru>
2019-03-27 21:19:38 +0400

    done homework lesson 8

---
+++ src/com/gb/algorithms/lesson8/Item.java
@@ -0,0 +1,13 @@
+package com.gb.algorithms.lesson8;
+
+public class Item {
+    private int data;
+
+    public Item(int data) {
+        this.data = data;
+    }
+
+    public int getKey(){
+        return this.data;
+    }
+}
